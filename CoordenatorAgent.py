28ddb9db5b563e72055cad093783502423ea1354
Cassiano Junior <cassianojuniorww@gmail.com>
2022-07-27 23:56:32 -0300

    Organizing coordenator

--- CoordenatorAgent.py
+++ CoordenatorAgent.py
@@ -7,11 +7,11 @@ from spade.template import Template
 from AdditionAgent import AdditionAgent
 from DivisionAgent import DivisionAgent
 from ExponentiationAgent import ExponentiationAgent
+from ExpressionAnalysis import separateString, solve_expression
+from makeMessage import makeMessage
 from MultiplyAgent import MultiplyAgent
 from SubtractionAgent import SubtractionAgent
 
-from ExpressionAnalysis import solve_expression, separateString
-from makeMessage import makeMessage
 
 class CoordenatorAgent(Agent):
     class CoordenatorBehav(CyclicBehaviour):
@@ -23,7 +23,8 @@ class CoordenatorAgent(Agent):
             # Tratar a expressão para saber qual agente chamar
 
             #Resultado = 25
-            data = separateString("(100 – 413 * (20 – 5 * 4) + 25) / 5")
+            # data = separateString("(100 – 413 * (20 – 5 * 4) + 25) / 5")
+            data = separateString("5 - 3")
 
             #Resultado = 32
             # data = separateString("27 + (14 + 3 * (100 / (18 – 4 * 2) + 7) ) / 13")
@@ -35,10 +36,10 @@ class CoordenatorAgent(Agent):
             # data = separateString("25 + (14 – (25 * 4 + 40 – 20))")
 
             print(data)
-            
+
             while(len(data) > 1):
                 operands = solve_expression(data)
-                    
+
                 onlyNumbersData = f"{operands['x1']} {operands['x2']}"
 
                 # Enviar mensagem para o respectivo agente
@@ -69,8 +70,8 @@ class CoordenatorAgent(Agent):
                     del(data[operands['n'] - 1])
                     print(f"Answer: {data}")
                     # self.run(str(data).strip('[]'))
-                    # await self.agent.stop()
-            print(f"================FINAL ANSWER: {data}================")
+            print(f"================ FINAL ANSWER: {float(data[0])} ================")
+            await self.agent.stop()
 
     async def setup(self):
         print("CoordenatorAgent started")
@@ -80,31 +81,10 @@ class CoordenatorAgent(Agent):
         self.add_behaviour(informBehav)
 
 
-class ReceiverAgent(Agent):
-    class RecvBehav(CyclicBehaviour):
-        async def run(self):
-            print("RecvBehav running")
-
-            msg = await self.receive(timeout=10)
-            if msg:
-                print(f"Message received with content: {msg.body}")
-                await self.agent.stop()
-            else:
-                print("Did not received any message after 10 seconds")
-
-            # stop agent from behaviour
-
-    async def setup(self):
-        print("ReceiverAgent started")
-        recvBehav = self.RecvBehav()
-        template = Template()
-        template.set_metadata("performative", "inform")
-        self.add_behaviour(recvBehav, template)
-
-
 if __name__ == "__main__":
     senderagent = CoordenatorAgent("coordenatoragent@anoxinon.me", "coordenator")
-    senderagent.start()
+    future = senderagent.start()
+    future.result()
 
     senderagent.web.start(hostname="127.0.0.1", port="2")
 
